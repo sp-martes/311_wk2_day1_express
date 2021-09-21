b58e5ac66e5d39d0e598a9a75c1b7d913a1e3b42
sp-martes <spencermartes@gmail.com>
2021-09-21 13:32:01 -0500

    wrapped up

--- index.js
+++ index.js
@@ -6,70 +6,69 @@ const port = process.env.PORT || 4000
 const { users } = require('./state')
 let count = users.length +1
 app.use(bodyParser.json())
+
 app.get("/users", function(req,res){
     res.json(users) 
 })
 
 app.get("/users/:id", function (req, res) {
-    let ID =  req.params.id -1
-    let chosenUser = users[ID] 
-       
-    if(!chosenUser){
+    let chosen1 = users.find(user => user._id == req.params.id) 
+    if(!chosen1){
         res.status(404).send("User not found")
     } 
-    res.json(chosenUser) 
+    res.json(chosen1) 
 });
 
-
-// app.post("/users", function (req,res){
-//     let addedUser = {
-//         "_id": count,
-//         "name": "Old Gregg",
-//         "occupation": "lake dweller",
-//         "avatar": "https://static.wikia.nocookie.net/mightyboosh/images/b/b6/Old_Gregg.jpg/revision/latest?cb=20090417120306"
-//     }
-//     let found = users.find(user => user.name == addedUser.name);
-//     if(found){
-//         return res.send('user already exists')
-//     }
-//     count ++;
-//     users.push(addedUser);
-//     res.json(users.slice(-1));
-// });
 
 app.post("/users", function (req,res){
-    console.log(req.body);
-    let newUser = {
+    let addedUser = {
         "_id": count,
-        "name": req.body.name,
-        "occupation": req.body.occupation,
-        "avatar": req.body.avatar
+        "name": "Old Gregg",
+        "occupation": "lake dweller",
+        "avatar": "https://static.wikia.nocookie.net/mightyboosh/images/b/b6/Old_Gregg.jpg/revision/latest?cb=20090417120306"
+    }
+    let found = users.find(user => user.name == addedUser.name);
+    if(found){
+        return res.send('user already exists')
     }
     count ++;
-    users.push(newUser);
-    res.json(users)
+    users.push(addedUser);
+    res.json(users.slice(-1));
 });
 
+// Looks like bodyparser didn't work. Deprecated?
+
+// app.post("/users", function (req,res){
+//     console.log(req.body);
+//     let newUser = {
+//         "_id": count,
+//         "name": req.body.name,
+//         "occupation": req.body.occupation,
+//         "avatar": req.body.avatar
+//     }
+//     count ++;
+//     users.push(newUser);
+//     res.json(users)
+// });
+
 app.put("/users/:id", function (req, res) {
-    let ID =  req.params.id -1
-    let chosenUser = users[ID] 
-    chosenUser.occupation = 'classified'
+    let chosen1 = users.find(user => user._id == req.params.id) 
+    chosen1.occupation = 'classified'
     
-    res.json(chosenUser) 
+    res.json(chosen1) 
 });
 
 // app.delete("/users/:id", function(req,res){
-//     let ID =  req.params.id -1
-//     let chosenUser = users[ID] 
-//     users.splice(ID,1)
+//     let chosen1 = users.find(user => user._id == req.params.id) 
+//     // let ID =  req.params.id -1
+//     users.splice(chosen1._id -1,1)
 //     res.json(users)
 // });   
 
 app.delete("/users/:id", function(req,res){
-    let ID =  req.params.id -1
-    let chosenUser = users[ID] 
-    chosenUser.isActive = false;
-    res.send('deleted')
+    let chosen1 = users.find(user => user._id == req.params.id) 
+    chosen1.isActive = false;
+    res.send('user deleted')
 });   
 
 app.listen(port, () => 
